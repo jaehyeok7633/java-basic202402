3ce5697ee5b77a88975684bab5ba525afd422de8
Jaehyeok <jaehyeok7633@naver.com>
2024-02-27 17:06:32 +0900

    Java oop(다형성 캐스팅, instanceof)

--- src/oop/poly/car/Tesla.java
+++ src/oop/poly/car/Tesla.java
@@ -7,4 +7,13 @@ public class Tesla extends Car {
         System.out.println("테슬라가 달립니다~");
     }
 
+    public void activeAutopilot() {
+        System.out.println("테슬라의 오토파일럿을 가동합니다!");
+    }
+
 }
+
+
+
+
+
