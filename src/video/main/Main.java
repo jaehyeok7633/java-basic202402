825b737fca2d8d8c661a66865d52d4f5202214dc
Jaehyeok <jaehyeok7633@naver.com>
2024-03-07 15:31:51 +0900

    DVD 관리 프로그램 기본 틀 완성

--- src/video/main/Main.java
+++ src/video/main/Main.java
@@ -2,21 +2,29 @@ package video.main;
 
 import video.ui.AppUi;
 
-import static video.ui.AppUi.inputInteger;
-import static video.ui.AppUi.startScreen;
+import static video.ui.AppUi.*;
 
 public class Main {
+
     public static void main(String[] args) {
 
         AppController controller = new AppController();
 
         while (true) {
-            startScreen();;
+            startScreen();
             int selectNumber = inputInteger(">>> ");
             controller.chooseSystem(selectNumber);
         }
 
-
-
     }
+
 }
+
+
+
+
+
+
+
+
+
